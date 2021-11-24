33090838e2b6efc5b446252bdde040fbf10b73f6
Gianluca Benassi <benox1199@gmail.com>
2021-11-24 15:16:49 +0100

    add play button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-// loads random numbers
-const numbersBox = document.getElementById('number-box');
-const rndNumbers = rnd5Numbers();
+// load page elements
+loadPage();
 
-loadNum(numbersBox, rndNumbers);
 
-//load function after 30 secons
-setTimeout(startGame, 30000, rndNumbers)
+// on click btn
+const btnPlay = document.getElementById('play');
+btnPlay.addEventListener('click', function(){
+    
+    // hide button
+    this.classList.add('d-none');
+
+    // reload elements
+    loadPage();
+
+});
+
+
 
 
 // ***** functions *****
+
+
+// on load page
+function loadPage() {
+
+    // loads random numbers
+    const numbersBox = document.getElementById('number-box');
+    const rndNumbers = rnd5Numbers();
+    
+    loadNum(numbersBox, rndNumbers);
+    
+    //load function after 30 secons
+    setTimeout(startGame, 30000, numbersBox, rndNumbers)
+
+}
+
 
 // generates 5 randon numbers between 1 and 100
 function rnd5Numbers() {
@@ -50,10 +75,10 @@ function loadNum(container, numArray) {
 
 
 // hide numbers and ask numbers to user
-function startGame(computerNum) {
+function startGame(container, computerNum) {
 
     // hide numbers
-    numbersBox.classList.add('d-none');
+    container.classList.add('d-none');
 
     setTimeout(function(){
         
@@ -75,8 +100,11 @@ function startGame(computerNum) {
         messageBox.innerHTML = `Hai indovinato ${userCorrectNum.length} numeri su 5`;
     
         // load correct numbers
-        loadNum(numbersBox, userCorrectNum);
-        numbersBox.classList.remove('d-none');
+        loadNum(container, userCorrectNum);
+        container.classList.remove('d-none');
+
+        // show play button
+        btnPlay.classList.remove('d-none');
 
     }, 100);
 
